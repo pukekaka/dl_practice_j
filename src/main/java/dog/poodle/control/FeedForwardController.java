555484db3d41c02e80194172e8f935782d9d871b
pukekakaster <pukekakaster@gmail.com>
2017-10-13 16:02:34 +0900

    int-ntm

---
+++ src/main/java/dog/poodle/control/FeedForwardController.java
@@ -0,0 +1,7 @@
+package dog.poodle.control;
+
+/**
+ * Created by pk on 2017-10-13.
+ */
+public class FeedForwardController {
+}
---
+++ src/main/java/dog/poodle/intm.java
@@ -0,0 +1,7 @@
+package dog.poodle;
+
+/**
+ * Created by pk on 2017-10-13.
+ */
+public interface intm {
+}
